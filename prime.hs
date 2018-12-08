0dd52da0285eca812a892b0ae5b7bb02fe8c3214
danimal4580 <danielknight2019@u.northwestern.edu>
2018-12-08 15:47:32 -0600

    commiting new error message

--- prime.hs
+++ prime.hs
@@ -10,6 +10,6 @@ ldf k n | divides k n = k
         | otherwise = ldf (k+1) n
 
 prime0 :: Integer -> Bool
-prime0 n | n < 1 = error "not a positive integer"
+prime0 n | n < 1 = error "this was so fucking dumb"
          | n == 1 = False
          | otherwise = ld n == n

a27417fe3d59de12667b202f8a612a7f37bf19ad
danimal4580 <danielknight2019@u.northwestern.edu>
2019-01-21 15:15:10 -0600

    chapter6

---
+++ learnyouahaskell/hof.hs
@@ -0,0 +1,13 @@
+:multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+zipWhip' :: (a -> b ->c) -> [a] -> [b] -> [c]
+zipWhip' _ [] _ = []
+zipWhip' _ _ [] = []
+zipWhip' f (x:xs) (y:ys) = f x y : zipWhip' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+   where g x y = f y x
+
+   

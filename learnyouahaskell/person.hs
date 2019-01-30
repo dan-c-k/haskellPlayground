f394e150231ad7a535cc3be9f2d41e3c5f6452c5
danimal4580 <danielknight2019@u.northwestern.edu>
2019-01-30 15:02:38 -0600

    a lot of stuf

--- learnyouahaskell/person.hs
+++ learnyouahaskell/person.hs
@@ -1,10 +1,38 @@
 data Person = Person { firstName :: String  
                      , lastName :: String  
                      , age :: Int  
-                     , height :: Float  
-                     , phoneNumber :: String  
-                     , flavor :: String  
-                     } deriving (Show)  
+                     } deriving (Eq, Show, Read) 
+
+data Vector a = Vector a a a deriving (Show)
+
+vPlus :: (Num t) => Vector t -> Vector t -> Vector t
+vPlus (Vector i j k) (Vector x y z) = Vector (i+x) (j+y) (k+z)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday   
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)  
 
 -- left off at the vector example in learn you a haskell
 -- i'm unclear on the examples involinvg type vs value contructor
+
+-- leaving off on recursive data types
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)  
+
+singleton :: a -> Tree a  
+singleton x = Node x EmptyTree EmptyTree  
+  
+treeInsert :: (Ord a) => a -> Tree a -> Tree a  
+treeInsert x EmptyTree = singleton x  
+treeInsert x (Node a left right)   
+    | x == a = Node x left right  
+    | x < a  = Node a (treeInsert x left) right  
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool  
+treeElem x EmptyTree = False  
+treeElem x (Node a left right)  
+    | x == a = True  
+    | x < a  = treeElem x left  
+    | x > a  = treeElem x right  
---
+++ learnyouahaskell/test.hs
@@ -0,0 +1,7 @@
+main = do 
+    putStrLn . show $ test 2
+
+
+test :: Int -> Int
+test x = x + 1
+    
